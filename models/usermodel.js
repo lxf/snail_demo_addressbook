192fe5c4faa0422232a12ed73fd452df109c7fb3
upsnail <code_li@hotmail.com>
2015-05-19 15:19:29 +0800

    增加了package.json

--- models/usermodel.js
+++ models/usermodel.js
@@ -1,3 +1,4 @@
+var crypto = require('crypto');
 var mongodb = require('./mongodb');
 var Schema = mongodb.mongoose.Schema;
 var ObjectId = Schema.ObjectId;
@@ -13,26 +14,46 @@ var UserSchema = new Schema({
     school_year: { type: Number },
     school: { type: String },
     major: { type: String },
-    description: { type: String }
+    description: { type: String },
+    isEdit: { type: Boolean }//用户只能有一次机会修改学校 学年 专业等
 });
 
 
 var User = mongodb.mongoose.model("UserModel", UserSchema);
 
+function User(user) {
+    this.nickname = user.nickname;
+    this.account = user.account;
+    this.email = user.email;
+    this.phone = user.phone;
+    this.pwd = user.pwd;
+    this.school_year = user.school_year;
+    this.school = user.school;
+    this.major = user.major;
+    this.realname = user.realname;
+    this.description = user.description;
+}
+
 var UserDAO = function () { };
 
-UserDAO.prototype.Reg = function (account, realname, nickname, email, phone, pwd, school_year, school, major,description, callback) {
-    var usermodel = new User();
-    usermodel.nickname = nickname;
-    usermodel.account = account;
-    usermodel.email = email;
-    usermodel.phone = phone;
-    usermodel.pwd = pwd;
-    usermodel.school_year = school_year;
-    usermodel.school = school;
-    usermodel.major = major;
-    usermodel.realname = realname; 
-    usermodel.description = description;
+UserDAO.prototype.Reg = function (account, realname, nickname, email, phone, pwd, school_year, school, major, description, callback) {
+    var md5 = crypto.createHash('md5');
+
+    var user = {
+        nickname: nickname,
+        account: account,
+        email: email,
+        phone: phone,
+        pwd: md5.update(pwd).digest('hex'),
+        school_year: school_year,
+        school: school,
+        major: major,
+        realname: realname,
+        description: description
+    };
+
+    var usermodel = new User(user);
+
     //判断是否存在同样的帐号或者邮箱
     usermodel.save(callback);
 }

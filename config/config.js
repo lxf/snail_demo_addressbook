b782419616887f3fed68b7cc7a2f49e761f54002
lxf <code_li@hotmail.com>
2015-05-24 17:07:49 +0800

    Update config.js

--- config/config.js
+++ config/config.js
@@ -11,20 +11,20 @@ var config = {
     cookiekey:'cookiename',
     host: 'localhost',
     // mongodb 配置
-    db: 'mongodb://snail:123465@ds059938.mongolab.com:59938/upsnail',
+    db: '',
     // redis 配置，默认是本地
     redis_host: '127.0.0.1',
     redis_port: 6379,
     // 程序运行的端口
     port: 8888,
-    host:'120.27.38.175',
+    host:'',
     // 邮箱配置
     mail_opts: {
         host: 'smtp.126.com',
         port: 25,
         auth: {
             user: 'mspublic@126.com',
-            pass: 'upsnail'
+            pass: ''
         }
     }
 };

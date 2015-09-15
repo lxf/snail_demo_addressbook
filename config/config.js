dd9f0bb43216de1758eefb2776bc137cb2768479
upsnail <code_li@hotmail.com>
2015-09-15 17:30:27 +0800

    change solution name

--- config/config.js
+++ config/config.js
@@ -1,23 +1,21 @@
 ﻿/**
  *配置文件
  */
-
 var path = require('path');
 var config = {
-    app_name: '十年通讯录',
-    app_description: '通讯录',
-    app_keywords: '通讯录',
+    app_name: 'JIT Lab System',
+    app_description: 'Manage Demo',
+    app_keywords: 'Demo',
     cookieSecret: 'upsnail',
     cookiekey: 'cookiename',
     host: 'localhost',
-    // mongodb 配置
-    // db: 'mongodb://snail:123465@ds059938.mongolab.com:59938/upsnail',
-    db:'mongodb://127.0.0.1/upsnail',
+    db: 'mongodb://snail:123465@ds059938.mongolab.com:59938/upsnail',
+    //db:'mongodb://127.0.0.1/upsnail',
     // redis 配置，默认是本地
     redis_host: '127.0.0.1',
     redis_port: 6379,
     // 程序运行的端口
-    port: 1234,
+    port: 18080,
     host: '127.0.0.1',
     // 邮箱配置
     mail_opts: {
